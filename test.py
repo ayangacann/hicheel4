80b7ec3236944d3ed0597d8878647a080b9ba0da
Cann <43157847+ayangacann@users.noreply.github.com>
2019-08-25 18:07:27 +0800

    Гэрт хийсэн даалгавар

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-# 5-Бодлого
-n=int(input())
-m=n//60
-s=n%60
-print(m, ":" ,s)
+n = int(input())
+a = n % 10
+b = n // 10 % 10
+c = n // 100 % 10
+d = n // 1000 % 10
+e = n // 10000
+print(a + b + c + d + e)
