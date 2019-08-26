f3d09ed18928f106b928d873aba5ff00764c079c
Cann <43157847+ayangacann@users.noreply.github.com>
2019-08-26 21:21:33 +0800

    Folder-т өөрчлөлт хийв

---
+++ Dasgaluud/7.py
@@ -0,0 +1,7 @@
+# Өгөгдсөн тоо эерег тэгш тоо бол true үгүй бол false гэж хэвлэ.
+
+n = int(input())
+if (n > 0 and n % 2 == 0):
+    print(True)
+else:
+    print(False)
--- if else.py
+++
@@ -1,7 +0,0 @@
-a = int(input())
-b = int(input())
-if a > b:
-    ih = a
-else:
-    ih = b
-print(ih)
